cdf1fa7fccdb38998433d827c1791f4999e9d5e3
Eugene FILIN <eafilin@mts.ru>
2025-01-25 11:49:27 +0300

    Add verbose flag

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Println("Usage:   ./busyscout local_file remote_path")
+	argsCount := len(os.Args)
+	if argsCount < 3 || argsCount > 4 || argsCount == 4 && os.Args[3] != "--verbose" {
+		fmt.Println("Usage:   ./busyscout local_file remote_path [--verbose]")
 		fmt.Println("Example: ./busyscout ipwiz.zip root:12345@192.168.10.18:/tmp")
 		os.Exit(0)
 	}
@@ -17,8 +18,9 @@ func main() {
 	// Extract the source and target file paths from command line arguments
 	sourceFile := os.Args[1]
 	targetFile := os.Args[2]
+	verboseFlag := argsCount == 4 && os.Args[3] == "--verbose"
 
-	s, errNew := scout.New(sourceFile, targetFile)
+	s, errNew := scout.New(sourceFile, targetFile, verboseFlag)
 	if errNew != nil {
 		klog.Fatal(errNew)
 	}
